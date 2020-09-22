c6d27e55487f391d0a85c78ab00017635c4f4137
Drevok <71374058+Drevok@users.noreply.github.com>
2020-09-22 21:41:51 +0200

    Introducing bars filling

--- Assets/Scripts/Tamagochi.cs
+++ Assets/Scripts/Tamagochi.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Propriétés
 {
     public class Tamagochi : MonoBehaviour
     {
+
+        [Header ("Stat bars")]
+        public Image HungerBar;
+        public Image TirednessBar;
+        public Image BoredomBar;
+ 
 
         public TextMeshProUGUI HungerText;
         public TextMeshProUGUI TirednessText;
@@ -38,6 +45,11 @@ namespace Propriétés
             Debug.Log("Il me reste " + tama.Hunger + "points de faim");
             Debug.Log("Il me reste " + tama.Tiredness + "points de fatigue");
             Debug.Log("Il me reste " + tama.Boredom + "points de fatigue");
+
+            HungerBar.fillAmount = tama.Hunger;
+            TirednessBar.fillAmount = tama.Tiredness;
+            BoredomBar.fillAmount = tama.Boredom;
+
         }
 
         public TAMA tama = new TAMA();
@@ -45,13 +57,13 @@ namespace Propriétés
         {
 
             //tama.Health = tama.MaxHealth;
-            tama.Health = 55;
+            tama.Health = 100;
             //tama.Hunger = tama.MaxHunger;
-            tama.Hunger = 55;
+            tama.Hunger = 100;
             //tama.Tiredness = tama.MaxTiredness;
-            tama.Tiredness = 10;
+            tama.Tiredness = 100;
             //tama.Boredom = tama.MaxBoredom;
-            tama.Boredom = 10;
+            tama.Boredom = 100;
 
             InvokeRepeating ("TimeCount", 0f, 1f);
 
